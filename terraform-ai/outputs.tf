99888fb762e283e69f942c1cf997b81dc2f2974c
halo <dongjin778@naver.com>
2026-02-14 09:35:34 +0900

    feat/infra: release 환경 구축 완료

---
+++ terraform-ai/outputs.tf
@@ -0,0 +1,13 @@
+output "parameter_arns" {
+  description = "ARNs of created SSM parameters"
+  value = {
+    for k, v in aws_ssm_parameter.parameters : k => v.arn
+  }
+}
+
+output "parameter_names" {
+  description = "Names of created SSM parameters"
+  value = {
+    for k, v in aws_ssm_parameter.parameters : k => v.name
+  }
+}
